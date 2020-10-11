e41b4eb219937780e317a14bc5462bacc74ce897
jvh <julianvehe@unisabana.edu.co>
2020-10-10 22:33:37 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,9 +1,7 @@
+const mongoose = require('mongoose');
 
-  const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://julian31079:1234@cluster0.wm6mv.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useUnifiedTopology: true});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect("mongodb+srv://julian31079:1234@cluster0.wm6mv.gcp.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+})
+  .then(db => console.log(`DB is connected`))
+  .catch(err => console.error(err));
